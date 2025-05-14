f3f2f0c042cec65866c8df47d0ed40ddd2263f21
JINOJ37 <jinoj0423@gmail.com>
2025-05-15 00:33:51 +0900

    :sparkles: Feat : ID 토큰 인증 컨트롤러  생성 (#1)
    AuthController 생성

---
+++ src/main/java/com/shujinko/project/controller/AuthController.java
@@ -0,0 +1,33 @@
+package com.shujinko.project.controller;
+
+import com.google.firebase.auth.FirebaseAuthException;
+import com.shujinko.project.service.AuthService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collections;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+
+    @Autowired
+    private AuthService authService;
+
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody Map<String, String> request) {
+        String idToken = request.get("idToken");
+        try {
+            String jwt = authService.authenticate(idToken);
+            return ResponseEntity.ok(Collections.singletonMap("token", jwt));
+        } catch (FirebaseAuthException e) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid ID token");
+        }
+    }
+}
